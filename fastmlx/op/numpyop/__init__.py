07a5fd01519259f81aa441bf4d212953497389b8
Michael Potter <NanoTekGuy@Gmail.com>
2025-05-22 23:45:01 -0700

    Improve test logging and augmentation ops

--- fastmlx/op/numpyop/__init__.py
+++ fastmlx/op/numpyop/__init__.py
@@ -1,6 +1,26 @@
 """Simple preprocessing ops implemented with MLX."""
 
-from .univariate import ExpandDims, Minmax
+from .univariate import (
+    ExpandDims,
+    Minmax,
+    Normalize,
+    PadIfNeeded,
+    RandomCrop,
+    HorizontalFlip,
+    CoarseDropout,
+    Onehot,
+    Sometimes,
+)
 
-__all__ = ["ExpandDims", "Minmax"]
+__all__ = [
+    "ExpandDims",
+    "Minmax",
+    "Normalize",
+    "PadIfNeeded",
+    "RandomCrop",
+    "HorizontalFlip",
+    "CoarseDropout",
+    "Onehot",
+    "Sometimes",
+]
 
