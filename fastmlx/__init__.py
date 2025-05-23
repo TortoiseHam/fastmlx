6b3cabca741a782de9c70c16f414048d3b169ade
Michael Potter <NanoTekGuy@Gmail.com>
2025-05-23 00:20:54 -0700

    Add image display utilities

--- fastmlx/__init__.py
+++ fastmlx/__init__.py
@@ -4,8 +4,9 @@ from .pipeline import Pipeline
 from .network import Network
 from .estimator import Estimator
 from .util import build
+from .display import BatchDisplay, GridDisplay
 
-__all__ = ["Pipeline", "Network", "Estimator", "build", "__version__"]
+__all__ = ["Pipeline", "Network", "Estimator", "build", "BatchDisplay", "GridDisplay", "__version__"]
 
 # Package semantic version
 __version__: str = "0.1.0"
